62564ec9d370e4bbf10ff6f6ccffba93728f01a1
Naveen Kumar <naveenkumar@Mac-3.lan>
2026-03-01 01:08:42 +0530

    Updated frontend API to production backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8082/api",
+ baseURL: "https://mern-task-manager-uy2j.onrender.com/api",
 });
 
 export default api;
